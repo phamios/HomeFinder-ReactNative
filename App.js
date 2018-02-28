0681a338af454387ac4f37bc4b4ee6a29994475b
admin <admin@SonPhams-Retina.local>
2018-02-28 10:24:48 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
+
+  
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults },
+});
+
+export default App;
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
